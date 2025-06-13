825de22e46899bb6132f17d452ab212e618e0ec4
Matheus Victor <matheuszaya@gmail.com>
2025-06-13 14:53:51 -0400

    comparações

--- procedureCatalogo.sql
+++ procedureCatalogo.sql
@@ -1,3 +1,27 @@
+--Maria
+create procedure sp_tabelas_relacionadas (p_bd varchar(255), p_table varchar(255))
+begin
+	select constraint_schema as 'banco', referenced_table_name as 'tabela', table_name as 'relacionamento',  -- consulta o relacionamento em que a tabela está como chave estrangeira
+    referenced_column_name as 'lado 1', column_name as 'lado n'
+	from information_schema.key_column_usage
+	where constraint_schema = p_bd and referenced_table_name = p_table
+
+	union
+
+	select constraint_schema as 'banco', table_name as 'tabela',referenced_table_name as 'relacionamento', -- consulta o relacionamento em que outras tabelas estão como chave estrangeira
+    referenced_column_name as 'lado 1', column_name as 'lado n'
+	from information_schema.key_column_usage
+	where constraint_schema = p_bd and table_name = p_table;
+end $$
+delimiter ;
+
+
+
+
+
+
+
+
 CREATE PROCEDURE listar_relacionamentos(p_tabname VARCHAR(50))
 RETURNING VARCHAR(50), VARCHAR(50);
 DEFINE lado1 VARCHAR(50);
--- triggers.sql
+++ triggers.sql
@@ -1,3 +1,22 @@
+--Maria
+
+
+CREATE TRIGGER tg_atualizar_bairro_cliente AFTER UPDATE ON cliente
+FOR EACH ROW
+	BEGIN
+		IF NEW.clibaicodigo <> OLD.clibaicodigo THEN
+			UPDATE bairro SET baiqtdepessoas = baiqtdepessoas + 1
+			WHERE baicodigo = NEW.clibaicodigo;
+			
+			UPDATE bairro SET baiqtdepessoas = baiqtdepessoas - 1
+			WHERE baicodigo = OLD.clibaicodigo;
+		END IF;
+	END$$
+
+DELIMITER ;
+
+
+
 
 CREATE TRIGGER tg_atualizar_bairro_cliente
 AFTER UPDATE OF clibaicodigo ON cliente
@@ -13,6 +32,25 @@ WHEN (new.clibaicodigo != old.clibaicodigo)
     SET baiqtdepessoas = baiqtdepessoas - 1
     WHERE baicodigo = old.clibaicodigo;
 );
+
+
+
+
+
+
+
+
+--Maria
+
+CREATE TRIGGER tg_atualiza_saldo_produto AFTER INSERT ON itemvenda
+FOR EACH ROW
+	BEGIN
+		UPDATE produto
+        SET prosaldo = prosaldo - new.itvqtde
+        WHERE procodigo = NEW.itvprocodigo;
+    END $$
+
+DELIMITER ;
 
 
 
